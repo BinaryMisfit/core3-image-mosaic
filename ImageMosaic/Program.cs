6ccf4a559978c1bfb99633fa90957b8bc48cf933
Willie Roberts <wroberts@mrpg.com>
2020-08-03 16:11:31 +0200

    Initial project commit

---
+++ ImageMosaic/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ImageMosaic
+{
+    using System;
+    using System.CommandLine;
+    using System.CommandLine.Invocation;
+    using System.IO;
+
+    internal class Program
+    {
+        private static int Main(string[] args)
+        {
+            RootCommand commandLine = new RootCommand
+            {
+                new Argument<FileInfo>(
+                    "SourceImage",
+                    "Full path of the source image")
+                .ExistingOnly(),
+                new Argument<DirectoryInfo>(
+                    "ImagePath",
+                    "Path to folder containing the images to use for replacement")
+                .ExistingOnly()
+            };
+            commandLine.Description = "Generates a mosaic using a source image and selection of smaller images";
+            commandLine.Handler = CommandHandler.Create<FileInfo, DirectoryInfo>((sourceImage, imagePath) =>
+            {
+                SourceImageFile sourceImageFile = new SourceImageFile(sourceImage);
+                if (!sourceImageFile.IsImageFile())
+                {
+                    Console.WriteLine($"{sourceImageFile.FileName} is not a valid image file");
+                    Environment.Exit(255);
+                }
+            });
+            return commandLine.InvokeAsync(args).Result;
+        }
+    }
+}
---
+++ ImageMosaic/SourceImageFile.cs
@@ -0,0 +1,37 @@
+﻿namespace ImageMosaic
+{
+    using SixLabors.ImageSharp;
+    using SixLabors.ImageSharp.Formats;
+    using System.IO;
+
+    internal class SourceImageFile
+    {
+        private readonly FileInfo file;
+
+        public SourceImageFile(FileInfo file)
+        {
+            this.file = file;
+            if (file == null)
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        public string FileName => file.FullName;
+
+        public bool IsImageFile()
+        {
+            try
+            {
+                using (Image image = Image.Load(file.OpenRead(), out IImageFormat format))
+                {
+                    return true;
+                }
+            }
+            catch (UnknownImageFormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
